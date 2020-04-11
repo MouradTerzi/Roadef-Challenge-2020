1287dd47f429ed7ce0eab64c7b52ff3262d55530
hnaienfaicel <61979071+hnaienfaicel@users.noreply.github.com>
2020-04-11 22:43:56 +0200

    Update roadef_first_model.py

--- Mathematical_models/roadef_first_model.py
+++ Mathematical_models/roadef_first_model.py
@@ -7,6 +7,7 @@ resou_number = 1
 horizon = 3
 scenario = 3
 alpha = 0.5
+tau=0.5
 compl_time = 15
 
 #2. Declaration of the different model's initial data 
@@ -29,7 +30,7 @@ for i in range(inter_numbers):
 #Each column int r_c_i_t_t1[key] corresponds to a given t' leq than t 
 
 r_c_i_t_t1 = {
-    (0,0): [[3],[0],[8]], 
+    (0,0): [[31],[0],[8]], 
     (1,0): [[14],[0,14],[0,0,14]],
     (2,0): [[5],[0,5],[0,0]]
 }
@@ -61,6 +62,7 @@ q_t = model.addVars(horizon,vtype = GRB.CONTINUOUS, name ="q_t")
 excess = model.addVars(horizon,vtype = GRB.CONTINUOUS, name = "excess")
 
 #4. Add constraints 
+
 model.addConstrs((quicksum(z[i,t] for t in range(horizon)) == 1 for i in range(inter_numbers)))
 model.addConstrs((w[i, t] >= z[i, t] for i in range(inter_numbers) for t in range(horizon)))
 model.addConstrs((z[i, t]*(t + delta_i_t[i][t]) <= horizon for i in range(inter_numbers) \
@@ -81,10 +83,10 @@ for t in range(horizon)))
 model.addConstrs((risk_s_t[s,t] == quicksum(risk_s_i_t_t1[(i,s)][t][t1]*beta[i,t,t1] for i in range(inter_numbers) for t1 in range(t+1) if (i,s) in risk_s_i_t_t1 and \
 len(risk_s_i_t_t1[(i,s)][t]) > t1 ) for s in range(scenario) for t in range(horizon)))
 
-model.addConstrs((risk_bar_t[t] == quicksum(risk_s_t[s,t] for s in range(scenario))*round(1/scenario,2) for t in range(horizon)))
+model.addConstrs((risk_bar_t[t] == quicksum(risk_s_t[s,t] for s in range(scenario))*(1/scenario) for t in range(horizon)))
 
-model.addConstrs((quicksum(y[s,t] for s in range(scenario)) >= int(alpha*scenario) for t in range(horizon)))
-model.addConstrs((quicksum(y[s,t] for s in range(scenario)) <= int(alpha*scenario) +1 for t in range(horizon)))
+model.addConstrs((quicksum(y[s,t] for s in range(scenario)) >= tau*scenario for t in range(horizon)))
+model.addConstrs((quicksum(y[s,t] for s in range(scenario)) <= tau*scenario +1 for t in range(horizon)))
 
 model.addConstrs((risk_s_t[s,t] - risk_s_t[u,t]  + 1000*(1 + y[s,t] - y[u,t]) >= 0 for t in range(horizon) \
 for s in range(scenario) for u in range(scenario) if s != u) )
@@ -93,9 +95,16 @@ model.addConstrs((q_t[t] >= risk_s_t[s,t] - 1000*(1 - y[s,t]) for t in range(hor
 model.addConstrs(excess[t] >= 0 for t in range(horizon))
 model.addConstrs(excess[t] >= q_t[t] - risk_bar_t[t] for t in range(horizon))
 
+
+#contrainte rajouter par Faicel
+model.addConstrs( (quicksum(w[i,t1] for t1 in range(t, horizon)) >= z[i, t]*(delta_i_t[i][t]) for i in range(inter_numbers) \
+for t in range(horizon)))
+
+
 #Fix the objective
-obj = alpha*quicksum(risk_bar_t[t] for t in range(horizon))*round(1/horizon,2) + \
-(1 - alpha)*(quicksum(excess[t] for t in range(horizon))*(round(1/horizon,2)))
+obj = alpha*quicksum(risk_bar_t[t] for t in range(horizon))*(1/horizon) + \
+(1 - alpha)*(quicksum(excess[t] for t in range(horizon))*(1/horizon))
+
 
 model.setObjective(obj,GRB.MINIMIZE)
 
